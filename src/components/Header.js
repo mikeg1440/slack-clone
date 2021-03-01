d6dba39649dfa5540f788149cd06169813d39367
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-01 09:01:16 -0500

    Refactor header and add more content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,57 +1,75 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import {BsArrowLeftShort} from 'react-icons/bs';
 import {BsArrowRightShort} from 'react-icons/bs';
 import {AiOutlineClockCircle} from 'react-icons/ai';
 import {BiHelpCircle} from 'react-icons/bi';
+import {VscCircleFilled} from 'react-icons/vsc';
 
-export default function Header() {
-
-    const avatarImg = useRef();
-
-    useEffect(() => {
-
-        
-    }, [username])
+export default function Header({ username }) {
 
     return (
         <StyledHeader>
             <div>
-                <AiOutlineClockCircle className='float-right' />
-                <BsArrowRightShort className='float-right' />
-                <BsArrowLeftShort className='float-right' />
+                <VscCircleFilled style={{color: 'red'}} />
+                <VscCircleFilled style={{color: 'yellow'}} />
+                <VscCircleFilled style={{color: 'green'}} />
             </div>
 
-            <div>
+            <StyledSearchbar>
+                <BsArrowLeftShort />
+                <BsArrowRightShort />
+                <AiOutlineClockCircle />
+
                 <StyledInput type='text' name='search' placeholder='Search' />
-                <BiHelpCircle style={{float: 'right'}}/>
-            </div>
+                <BiHelpCircle/>
+            </StyledSearchbar>
 
-            <div>
-                <b>Username</b>
-                <img src="" alt="User Avatar" ref={avatarImg} />
-            </div>
+            <StyledUserContainer>
+                <b>{username}</b>
+                <StyledImg src={`https://identicon.rmhdev.net/${username}.png`} alt="User Avatar" />
+            </StyledUserContainer>
         </StyledHeader>
     )
 }
 
+
 const StyledHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    align-content: center;
     background-color: #2a0b2a;
     color: white;
-    display: grid;
-    grid-template-columns: 0.4fr 1.6fr;
-    grid-template-rows: 1fr;
-    grid-column-gap: 0px;
-    grid-row-gap: 0px;
-    padding: .5rem 1rem .5rem 0;
+    padding: .5rem .5rem;
+`
+
+const StyledSearchbar = styled.div`
+    min-width: 400px;
+    display: flex;
+    align-items: center;
+`
+
+const StyledUserContainer = styled.div`
+    float: right;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
 `
 
 const StyledInput = styled.input`
     border-radius: 5px;
     text-align: center;
-    margin-left: 1rem;
+    margin: 0 1rem;
     border: 1px solid #ffffff85;
     background-color: #381637;
-    width: 60%;
     color: white;
+    max-width: 600px;
+    min-width: 300px;
+    line-height: 1.5;
+`
+
+const StyledImg = styled.img`
+    width: 20px;
+    border: 2px solid black;
 `
