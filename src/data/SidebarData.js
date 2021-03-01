6ce82e073f3787d492a2d21a5b81b5b828bb6b9a
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-01 10:14:23 -0500

    Create a mockup data file with sitebar page data in it

---
+++ src/data/SidebarData.js
@@ -0,0 +1,52 @@
+import SaveIcon from '@material-ui/icons/Save';
+import SubjectIcon from '@material-ui/icons/Subject';
+import InsertCommentIcon from '@material-ui/icons/InsertComment';
+import AppsIcon from '@material-ui/icons/Apps';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import PagesIcon from '@material-ui/icons/Pages';
+import NewReleasesIcon from '@material-ui/icons/NewReleases';
+import PersonPinIcon from '@material-ui/icons/PersonPin';
+
+
+export const sidebarItems = [
+    {
+        icon: <SubjectIcon/>,
+        text: 'All Unreads'
+    },
+    
+    {
+        icon: <InsertCommentIcon/>,
+        text: 'Threads'
+    },
+    
+    {
+        icon: <NewReleasesIcon/>,
+        text: 'Mentions & reactions'
+    },
+    
+    {
+        icon: <DraftsIcon/>,
+        text: 'Drafts'
+    },
+    
+    {
+        icon: <SaveIcon/>,
+        text: 'Saved'
+    },
+    
+    {
+        icon: <PagesIcon/>,
+        text: 'Channels'
+    },
+    
+    {
+        icon: <AppsIcon/>,
+        text: 'People'
+    },
+    
+    {
+        icon: <PersonPinIcon/>,
+        text: 'Apps'
+    },
+    
+]
