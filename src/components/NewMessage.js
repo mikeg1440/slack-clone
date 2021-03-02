ec931f232b7e47ff8b531006b1d3cf5187435155
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-01 19:23:32 -0500

    Create a NewMessage component

---
+++ src/components/NewMessage.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import styled from 'styled-components'
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import FormatBoldIcon from '@material-ui/icons/FormatBold';
+import FormatItalicIcon from '@material-ui/icons/FormatItalic';
+import StrikethroughSIcon from '@material-ui/icons/StrikethroughS';
+import CodeIcon from '@material-ui/icons/Code';
+import LinkIcon from '@material-ui/icons/Link';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import FontDownloadIcon from '@material-ui/icons/FontDownload';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+
+export default function NewMessage() {
+    return (
+        <NewMessageContainer>
+            <InnerMessageContainer>
+                <InputContainer>
+                    <MessageInput placeholder='Message #chat-room-name' type="text"/>
+                </InputContainer>
+
+                <TextOptionBar>
+                    <FlashOnIcon/>
+                    <FormatBoldIcon/>
+                    <FormatItalicIcon/>
+                    <StrikethroughSIcon/>
+                    <CodeIcon/>
+                    <LinkIcon/>
+                    <FormatListNumberedIcon/>
+                    <MoreHorizIcon/>
+                    <FontDownloadIcon/>
+                    <SentimentSatisfiedIcon/>
+                    <AttachFileIcon/>
+                </TextOptionBar>
+                
+            </InnerMessageContainer>
+        </NewMessageContainer>
+    )
+}
+
+
+const NewMessageContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    border: 1px solid #523753;
+    padding: 1rem;
+`
+
+const InnerMessageContainer = styled.div`
+    border: 1px solid black;
+    border-radius: 5px;
+    width: 70%;
+`
+
+const MessageInput = styled.input`
+    width: 100%;
+    border-radius: 5px;
+    border: 0;
+    line-height: 2;
+    padding: .25rem;
+`
+
+const TextOptionBar = styled.div`
+    width: 100%;
+    border-top: 1px solid #523753;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: .5rem;
+`
+
+const InputContainer = styled.div`
+    padding: .25rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
