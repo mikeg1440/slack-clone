3f4972975d4d8c028298f039985be0c95b271ede
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-01 19:22:37 -0500

    Add themes to app using themeprovider

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,48 @@ import Chat from './Chat';
 import Login from './Login';
 import Header from './Header'
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import { useState } from 'react';
+
+
+
+const lightTheme = {
+  headerFG: 'white',
+  headerBG: '#2a0b2a',
+  sidebarFG: 'white',
+  sidebarBG: '#3d0e3e',
+  chatFG: 'black',
+  chatBG: 'white'
+}
+
+const darkTheme = {
+  headerFG: 'white',
+  headerBG: '#1b1b16',
+  sidebarFG: 'white',
+  sidebarBG: '#0e0e0e',
+  chatFG: '#3d0e3e',
+  chatBG: 'black'
+}
+
 
 function App() {
+
+  const [currentTheme, setCurrentTheme] = useState('light');
+
+  const switchTheme = () => {
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    setCurrentTheme(newTheme);
+  }
+
   return (
     <div>
       <Router> 
        <Switch>
          <Route path='/chat'>
-          <Header username='nick' />
-          <Chat/>
+            <ThemeProvider theme={currentTheme === 'light' ? lightTheme : darkTheme} >
+              <Header username='nick' themeCallback={switchTheme} />
+              <Chat/>
+            </ThemeProvider>
          </Route>
           
          <Route path='/'>
