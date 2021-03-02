5f1dfd912e67f96176f7240188ec7e55a7fa17a9
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-02 16:00:53 -0500

    Add props the components in chatpage

--- src/containers/ChatPage.js
+++ src/containers/ChatPage.js
@@ -2,11 +2,11 @@ import React from 'react'
 import Header from '../components/Header';
 import Chat from '../components/Chat';
 
-export default function ChatPage(props) {
+export default function ChatPage({ themeCallback, rooms}) {
     return (
         <>
-              <Header username='nick' themeCallback={props.themeCallback} />
-              <Chat/>
+              <Header username='nick' themeCallback={themeCallback} />
+              <Chat rooms={rooms} />
         </>
     )
 }
