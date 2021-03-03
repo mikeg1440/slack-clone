6e5ae878e2074b66a262a6988c42df79e84b72fd
Michael Gaudrau <gaudreaum01@gmail.com>
2021-03-03 12:17:14 -0500

    Add props to pass down to header and chat components in chatpage

--- src/containers/ChatPage.js
+++ src/containers/ChatPage.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Header from '../components/Header';
 import Chat from '../components/Chat';
 
-export default function ChatPage({ themeCallback, rooms}) {
+export default function ChatPage({ themeCallback, rooms, signOutCallback}) {
     return (
         <>
-              <Header username='nick' themeCallback={themeCallback} />
+              <Header username='nick' themeCallback={themeCallback} signOutCallback={signOutCallback} />
               <Chat rooms={rooms} />
         </>
     )
